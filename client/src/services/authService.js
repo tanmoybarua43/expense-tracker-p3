fcd3c5d9fd926c552cddc41cb663b513a5a9c39b
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2024-10-17 19:14:43 -0700

    auth chages in token generation

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 const API_URL = 'http://localhost:5050/api/auth/';
 
@@ -11,5 +12,24 @@ const register = async (userData) => {
   const response = await axios.post(`${API_URL}register`, userData);
   return response.data;
 };
+// Helper function to get user from token
+const getUserFromToken = (token) => {
+  if (!token) return null;
+  const decoded = jwtDecode(token);
+  return {
+    id: decoded.id,
+    email: decoded.email, // Ensure the token contains email
+    username: decoded.username, // Ensure the token contains username
+  };
+};
 
-export default { login, register };
+// export default { login, register, getUserFromToken };
+// Assign the exported object to a variable
+const authService = {
+  login,
+  register,
+  getUserFromToken,
+};
+
+// Explicitly export the variable
+export default authService;

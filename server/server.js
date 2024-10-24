db9c8d3c1ca9d1b3e21af64e488c39bcd8c9703c
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2024-10-24 02:00:06 -0700

    updated in server.js file

--- server/server.js
+++ server/server.js
@@ -11,12 +11,15 @@ connectDB();
 const app = express();
 // app.use(cors());
 app.use(cors({
-    origin: 'https://expense-tracker-p3.vercel.app'  // Your frontend URL
-  }));
+    origin: 'https://expense-tracker-p3.vercel.app',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    credentials: true  
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
-
+app.options('*', cors());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
